04f0f5da4271c442a5a90d158e3b44a76e5e7914
Peter Solodov <solodov@gmail.com>
2025-06-10 11:29:07 -0400

    Focus previously focused tab
    Previously the tab immediately to the left would be focused. With this
    change extension maintains history of focused tabs and on closing
    focuces the previous tab, resulding in better user experience.

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,28 @@
-// listen for messages
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-//        console.log(request, sender);
-        // check the message command for a close tab command
-        if (request.command === "close_tab")
-            chrome.tabs.remove(sender.tab.id);
+const tabFocusHistory = new Map();
 
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  tabFocusHistory.set(activeInfo.tabId, Date.now());
+});
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+  tabFocusHistory.delete(tabId);
+});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.command === "close_tab" && sender.tab) {
+      chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+        if (tabs[0]?.id === sender.tab.id) {
+          // Find the most recently focused tab (excluding current)
+          const sortedTabs = Array.from(tabFocusHistory.entries())
+                .filter(([tabId]) => tabId !== sender.tab.id)
+                .sort(([, a], [, b]) => b - a);
+          if (sortedTabs.length > 0) {
+            await chrome.tabs.update(sortedTabs[0][0], { active: true });
+          }
+        }
+        chrome.tabs.remove(sender.tab.id);
+      });
     }
+  }
 );
